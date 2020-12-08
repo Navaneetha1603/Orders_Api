e7c6e3ce373817bee643a2e8f866599dc6df2c77
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2020-12-08 10:23:58 +0530

    change patch to post

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -2,7 +2,7 @@ const orderController=require("../controllers/OrderController");
 const express=require("express");
 const router=express.Router();
 router.get('/getOrders',orderController.getOrders);
-router.patch('/createOrders',orderController.createOrders);
+router.post('/createOrders',orderController.createOrders);
 router.get('/getOrder/:userId',orderController.getUserId);
 router.get('/orderCount',orderController.counting);
 
